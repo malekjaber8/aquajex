c035e27ed064f3d4493c604ff71d0b4069a9b983
malekjaber8 <malekjaber955@gmail.com>
2026-09-25 16:10:29 +0100

    Deploy web build: show the RNE photo in the admin's commercial client view

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2021772360" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3750649000" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
